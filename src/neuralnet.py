5ecf0a59786bd5b11c625f3b47d312cd859533bc
The third meow <jsheaandcats@gmail.com>
2019-05-24 18:14:03 +1200

    reperpose neural net to classify edges from clear-edge-dataet

--- src/neuralnet.py
+++ src/neuralnet.py
@@ -1,42 +1,52 @@
+import pickle
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
+import torch.optim as optim
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(2, 6)
-        self.fc2 = nn.Linear(6, 1)
+        self.fc1 = nn.Linear(4, 6)
+        self.fc2 = nn.Linear(6, 6)
+        self.fc3 = nn.Linear(6, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 # Learn dataset
-xlearn = [torch.tensor([0., 1.]), torch.tensor([1., 1.]), torch.tensor([0., 0.])]
-ylearn = [torch.tensor([1.]), torch.tensor([0.]), torch.tensor([0.])]
+clear_edges = pickle.load(open('data/clear_edges_data.pickle', 'rb'))
+xlearn = [torch.tensor(shape_label_pair[0], dtype=torch.float) \
+    for shape_label_pair in clear_edges]
+ylearn = [torch.tensor([shape_label_pair[1]], dtype=torch.float) \
+    for shape_label_pair in clear_edges]
+
 
 # Don't let len(xlearn) not equal len(ylearn)
 assert (len(xlearn) == len(ylearn)), "no. labels != no. data"
 
+# Create neural net, loss function and optimizer
 net = Net()
 mse = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+optimizer = optim.SGD(net.parameters(), lr=0.004)
 
-for i in range(30):
-    inp = xlearn[i % len(xlearn)]
-    out = net(inp)
-    target = ylearn[i % len(ylearn)]
+for p in range(20):
+    avg_loss = 0
+    for i in range(50):
+        inp = xlearn[i % len(xlearn)].view(4)
+        out = net(inp)
+        target = ylearn[i % len(ylearn)]
 
-    loss = mse(out, target)
+        loss = mse(out, target)
+        avg_loss += loss
 
-    # attempt to graph it
-    print('-' * int(loss.item() * 30.))
+        net.zero_grad()
+        loss.backward()
 
-    net.zero_grad()
-    loss.backward()
-
-    optimizer.step()
+        optimizer.step()
+    avg_loss /= 100
+    print(avg_loss)
