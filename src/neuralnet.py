80315dc135690f952007fd7178bd90d2329d6cf1
The third meow <jsheaandcats@gmail.com>
2019-05-24 18:21:30 +1200

    improve commenting

--- src/neuralnet.py
+++ src/neuralnet.py
@@ -5,9 +5,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
+# The neural network
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        # Setup layers
         self.fc1 = nn.Linear(4, 6)
         self.fc2 = nn.Linear(6, 6)
         self.fc3 = nn.Linear(6, 1)
@@ -18,12 +20,15 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-# Learn dataset
+
+# Load dataset
 clear_edges = pickle.load(open('data/clear_edges_data.pickle', 'rb'))
-xlearn = [torch.tensor(shape_label_pair[0], dtype=torch.float) \
-    for shape_label_pair in clear_edges]
-ylearn = [torch.tensor([shape_label_pair[1]], dtype=torch.float) \
-    for shape_label_pair in clear_edges]
+
+# Format data
+xlearn = [torch.tensor(shape_label_pair[0], dtype=torch.float)
+          for shape_label_pair in clear_edges]
+ylearn = [torch.tensor([shape_label_pair[1]], dtype=torch.float)
+          for shape_label_pair in clear_edges]
 
 
 # Don't let len(xlearn) not equal len(ylearn)
@@ -35,18 +40,25 @@ mse = nn.MSELoss()
 optimizer = optim.SGD(net.parameters(), lr=0.004)
 
 for p in range(20):
-    avg_loss = 0
+    total_loss = 0
     for i in range(50):
+        # Reshape input
         inp = xlearn[i % len(xlearn)].view(4)
+
+        # Run input through neural network
         out = net(inp)
+
+        # Calculate loss
         target = ylearn[i % len(ylearn)]
-
         loss = mse(out, target)
-        avg_loss += loss
 
+        # Keep track of total loss
+        total_loss += loss
+
+        # Backprop network
         net.zero_grad()
         loss.backward()
-
         optimizer.step()
-    avg_loss /= 100
-    print(avg_loss)
+
+    # Calculate and print average loss
+    print(total_loss/50)
