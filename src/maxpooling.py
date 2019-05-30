2a9528a53d4e97fd205df63ffacb376372f9e58e
The third meow <jsheaandcats@gmail.com>
2019-05-30 21:45:01 +1200

    continue experiments with conv2d/maxpooling

--- src/maxpooling.py
+++ src/maxpooling.py
@@ -3,28 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-conv = nn.Conv1d(4, 2, 2)
-maxpool = nn.MaxPool1d(2, 2)
-
-x = torch.rand(1, 4, 4)
-print(x, end='\n\n')
-x = conv(x)
-print(x, end='\n\n')
-x = F.relu(x)
-print(x, end='\n\n')
-x = maxpool(x)
-print(x, end='\n\n')
-
-conv = nn.Conv1d(4, 4, 2)
-maxpool = nn.MaxPool1d(2, 2)
-
-
-x = torch.Tensor(
+BASE = torch.Tensor(
     [[[0., 1., 1., 0.],
       [0., 0., 0., 0.],
       [1., 0., 0., 1.],
       [0., 1., 1., 0.]]]
 )
+
+conv = nn.Conv1d(4, 4, 2, stride=2)
+maxpool = nn.MaxPool1d(2, 2)
+
+x = BASE
 print(x, end='\n\n')
 x = conv(x)
 print(x, end='\n\n')
@@ -33,4 +22,18 @@ print(x, end='\n\n')
 x = maxpool(x)
 print(x, end='\n\n')
 
-print(
+print()
+print()
+print()
+
+conv = nn.Conv1d(4, 4, 2, stride=1)
+maxpool = nn.MaxPool1d(2, 2)
+
+x = BASE
+print(x, end='\n\n')
+x = conv(x)
+print(x, end='\n\n')
+x = F.relu(x)
+print(x, end='\n\n')
+x = maxpool(x)
+print(x, end='\n\n')
